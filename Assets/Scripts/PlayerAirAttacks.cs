cb06bc9404db947493313c88309af04df64bce1e
Skiryy <84138927+Skiryy@users.noreply.github.com>
2024-05-08 11:05:53 +0200

    laptop to pc transfer

--- Assets/Scripts/PlayerAirAttacks.cs
+++ Assets/Scripts/PlayerAirAttacks.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
@@ -5,7 +6,13 @@ public class PlayerAirAttacks : MonoBehaviour
 {
     private CharacterMover characterMover; // Reference to the CharacterMover script
     public bool attackStatus = false;
-    public bool moveAttackStatus = false;
+    public float distanceToPlayer;
+    public GameObject enemy;
+    public GameObject handsL;
+    public GameObject handsR;
+    public Rigidbody rb;
+    private bool coroutineStarted = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,16 +20,56 @@ public class PlayerAirAttacks : MonoBehaviour
 
         // Find and store the CharacterMover script component
         characterMover = GetComponent<CharacterMover>();
+        rb = GetComponent<Rigidbody>();
+        handsL.SetActive(false);
+        handsR.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        distanceToPlayer = Vector3.Distance(rb.position, enemy.transform.position);
+        bool rotateValue = characterMover.characterRotate;
         // Check for left mouse button click (button index 0) and if not currently attacking
+        // Access the characterRotate variable from CharacterMover script
         if (Input.GetMouseButtonDown(0))
         {
-            // Access the characterRotate variable from CharacterMover script
-            bool rotateValue = characterMover.characterRotate;
+            Debug.Log(distanceToPlayer);
+                if (rotateValue == false)
+            {
+                handsL.SetActive(true);
+                StartCoroutine(DeactivateHandsAfterDelay(handsL));
+                if (distanceToPlayer < 5)
+                {
+                    Debug.Log("Attack");    
+                    enemyScript EnemyScript = enemy.GetComponent<enemyScript>();
+                    EnemyScript.health -= 1f;
+                }
+            }
+            else if (rotateValue == true)
+            {
+                {
+                    handsR.SetActive(true);
+                    StartCoroutine(DeactivateHandsAfterDelay(handsR));
+                    if (distanceToPlayer < 5)
+                    {
+                        Debug.Log("Attack");
+                        enemyScript EnemyScript = enemy.GetComponent<enemyScript>();
+                        EnemyScript.health -= 1f;
+                    }
+                }
+            }
+        }
+
+        IEnumerator DeactivateHandsAfterDelay(GameObject hands)
+        {
+            if (!coroutineStarted)
+            {
+                coroutineStarted = true;
+                yield return new WaitForSeconds(0.5f);
+                hands.SetActive(false);
+                coroutineStarted = false;
+            }
         }
     }
 }
--- Assets/Scripts/enemyScript.cs
+++ Assets/Scripts/enemyScript.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro; // Dit wordt toegevoegd omdat de text functie van unity zo heet.
 
 public class enemyScript : MonoBehaviour
 {
     public float health = 100f;
     public GameObject barrel;
+    public TextMeshProUGUI enemyHealthDisplay;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,7 @@ public class enemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        enemyHealthDisplay.text = "Enemy Health" + health; 
         if (health <= 0)
         {
             Death();
